d6018b22d62a538172a537ea8a2360a0138145fe
vboxuser <yasminwoodlock@gmail.com>
2024-10-24 15:59:56 +0000

    made buildLoadValues make sense

--- project/CPU.java
+++ project/CPU.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class CPU{
     public int cores;
     cpuInfo cpu;
+    String name;
 
-    public CPU(cpuInfo cpu, int cores){
+    public CPU(cpuInfo cpu, int cores, String name){
         this.cores = cores;
         this.cpu = cpu;
+        this.name = name;
     }
 
     private int getCpuIdle(int core){
@@ -39,6 +41,7 @@ public class CPU{
         return cpu.l3CacheSize();
     }
 
+    //gets all types of cache and puts it into a key/value storage
     public HashMap<String, Integer> getAllCache(){
         HashMap<String, Integer> caches = new HashMap<String, Integer>();
         String str;
@@ -55,23 +58,34 @@ public class CPU{
         return caches;
     }
 
-    public void buildChartValues(){
+    //returns the percentage of time the CPU spends being not idle
+    private float loadPercent(int core){
+        int[] vals = getCoreStats(core);
+
+        int totalLoad = vals[0] + vals[1] + vals[2];
+        int busyLoad = vals[1] + vals[2];
+
+        return ((float)busyLoad/totalLoad) * 100;
+    }
+
+    //shows the load of a specified core
+    public void buildLoadValues(int type){
         //temporary timer
         int count = 0;
-        int idle;
+        int core = 0; //change to be mutable
 
         List<Integer> chartValues = new ArrayList<Integer>();
 
         while (count <10){
-            int[] vals = getCoreStats(0);
-            idle = vals[0];
-
+            float val = loadPercent(core);
 
             System.out.println(chartValues);
             if(chartValues.size() == 5){
                 chartValues.removeFirst();
             }
-            chartValues.add(idle);
+
+            //originally a float, rounded to be an int
+            chartValues.add(Math.round(val));
             count++;
         }
     }
--- project/Main.java
+++ project/Main.java
@@ -7,11 +7,15 @@ public class Main {
 
         cpuInfo cpu = new cpuInfo();
         cpu.read(0);
+        String name = cpu.getModel();
 
-        CPU help = new CPU(cpu, cpu.coresPerSocket());
-        help.buildChartValues();
-        int i = 0;
+        CPU help = new CPU(cpu, cpu.coresPerSocket(), name);
+
+        System.out.println("core 1");
+        help.buildLoadValues(0);
+
         help.getAllCache();
+        int i = 0;
 
     }
 }
